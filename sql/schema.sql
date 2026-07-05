a413df67a35300d9b1e150681b71ffcaf760e743
michaelbowser <michael@michaelbowser.org>
2026-07-04 20:42:44 -0500

    setting up db

---
+++ sql/schema.sql
@@ -0,0 +1,33 @@
+-- Drop tables if they already exist (development only)
+--
+DROP TABLE IF EXISTS owned_comics CASCADE;
+DROP TABLE IF EXISTS issues CASCADE;
+DROP TABLE IF EXISTS series CASCADE;
+DROP TABLE IF EXISTS publishers CASCADE;
+DROP TABLE IF EXISTS boxes CASCADE;
+
+-- Publishers  
+CREATE TABLE publishers (
+    ...
+);
+
+-- Series 
+CREATE TABLE series (
+    ...
+);
+
+-- Issues 
+CREATE TABLE issues (
+    ...
+);
+
+-- Boxes 
+CREATE TABLE boxes (
+    ...
+);
+
+-- Owned Comics 
+CREATE TABLE Owned Comics(
+    ...
+);
+
